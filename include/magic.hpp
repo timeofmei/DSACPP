ebe55c473a751f90283fa11a4bac0f352d50c4fc
timeofmei <timeofmei@outlook.com>
2022-11-22 09:44:48 +0800

    move reverse to magic

--- include/magic.hpp
+++ include/magic.hpp
@@ -9,4 +9,20 @@ void swap(T* a, T* b)
     *b = tmp;
 }
 
+template<typename T>
+void reverse(T A[], int lo, int hi)
+{
+    if (lo < hi)
+    {
+        swap(A + lo, A + hi);
+        reverse(A, lo + 1, hi - 1);
+    }
+}
+
+template<typename T>
+void reverse(T A[], int length)
+{
+    reverse(A, 0, length - 1);
+}
+
 #endif
